c41d312ba05be8158a472282375c58eea4d53791
Ignacio Salcedo <ignacio@isalcedo.com>
2017-11-20 07:27:38 -0500

    Initial

---
+++ FilemanagerAsset.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace dpodium\filemanager;
+
+use yii\web\AssetBundle;
+
+class FilemanagerAsset extends AssetBundle {
+
+    public $sourcePath = '@dpodium/filemanager/assets';
+    public $css = [
+        'css/filemanager.css'
+    ];
+    public $js = [
+        'js/filemanager.js',
+    ];
+    public $depends = [
+        'yii\web\JqueryAsset',
+        'yii\bootstrap\BootstrapAsset',
+        'kartik\file\FileInputAsset',
+        'kartik\editable\EditableAsset',
+        'kartik\select2\Select2Asset'
+    ];
+
+    /**
+     * uncomment in localhost for debug purpose
+     */
+//    public $publishOptions = [
+//        'forceCopy' => true
+//    ];
+}
