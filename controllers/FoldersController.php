c15c14582bff8e421e01bda11c89b0b54682fc3b
Ignacio Salcedo <ignacio@isalcedo.com>
2019-01-30 10:36:24 -0500

    Remove Folder and some other admin fixes

--- controllers/FoldersController.php
+++ controllers/FoldersController.php
@@ -1,187 +1,211 @@
 <?php
 
-	namespace isalcedo\filemanager\controllers;
+namespace isalcedo\filemanager\controllers;
 
-	use Yii;
-	use yii\data\ActiveDataProvider;
-	use yii\web\Controller;
-	use yii\web\NotFoundHttpException;
-	use yii\filters\VerbFilter;
-	use isalcedo\filemanager\FilemanagerAsset;
+use Yii;
+use yii\data\ActiveDataProvider;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+use isalcedo\filemanager\FilemanagerAsset;
 
-	/**
-	 * FoldersController implements the CRUD actions for Folders model.
-	 */
-	class FoldersController extends Controller
-	{
+/**
+ * FoldersController implements the CRUD actions for Folders model.
+ */
+class FoldersController extends Controller
+{
 
-		public function behaviors()
-		{
-			return [
-				'verbs' => [
-					'class'   => VerbFilter::className(),
-					'actions' => [
-						'delete' => ['post'],
-					],
-				],
-			];
-		}
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class'   => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['post'],
+                ],
+            ],
+        ];
+    }
 
-		/**
-		 * Lists all Folders models.
-		 * @return mixed
-		 */
-		public function actionIndex()
-		{
-			FilemanagerAsset::register($this->view);
-			$model        = new $this->module->models['folders'];
-			$dataProvider = new ActiveDataProvider([
-				'query' => $model->find(),
-			]);
+    /**
+     * Lists all Folders models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        FilemanagerAsset::register($this->view);
+        $model        = new $this->module->models['folders'];
+        $dataProvider = new ActiveDataProvider([
+            'query' => $model->find(),
+        ]);
 
-			return $this->render('index', [
-				'dataProvider' => $dataProvider,
-				'model'        => $model
-			]);
-		}
+        return $this->render('index', [
+            'dataProvider' => $dataProvider,
+            'model'        => $model
+        ]);
+    }
 
-		/**
-		 * Displays a single Folders model.
-		 *
-		 * @param integer $id
-		 *
-		 * @return mixed
-		 */
-		public function actionView($id)
-		{
-			FilemanagerAsset::register($this->view);
+    /**
+     * Displays a single Folders model.
+     *
+     * @param integer $id
+     *
+     * @return mixed
+     */
+    public function actionView($id)
+    {
+        FilemanagerAsset::register($this->view);
 
-			return $this->render('view', [
-				'model' => $this->findModel($id),
-			]);
-		}
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
 
-		/**
-		 * Creates a new Folders model.
-		 * If creation is successful, the browser will be redirected to the 'view' page.
-		 * @return mixed
-		 */
-		public function actionCreate()
-		{
-			FilemanagerAsset::register($this->view);
-			$model = new $this->module->models['folders'];
+    /**
+     * Creates a new Folders model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+        FilemanagerAsset::register($this->view);
+        $model = new $this->module->models['folders'];
 
-			if ($model->load(Yii::$app->request->post()))
-			{
-				$model->storage       = isset($this->module->storage['s3']) ? 'S3' : 'local';
-				$model->parent_folder = trim($model->parent_folder, '/');
-				$model->path          = trim($model->path, '/');
+        if ($model->load(Yii::$app->request->post())) {
+            $model->storage       = isset($this->module->storage['s3']) ? 'S3' : 'local';
+            $model->parent_folder = trim($model->parent_folder, '/');
+            $model->path          = trim($model->path, '/');
 
-				if ($model->save())
-				{
-					if ($this->module->public_path)
-					{
-						if (!file_exists(Yii::getAlias('@webroot' . '/../..' . $this->module->public_path . $model->parent_folder . '/' . $model->path))) {
-							mkdir(Yii::getAlias('@webroot' . '/../..' . $this->module->public_path . $model->parent_folder . '/' . $model->path), 0755, true);
-						}
-					}
-					else
-					{
-						if (!file_exists(Yii::getAlias('@webroot' . $this->module->public_path . $model->parent_folder . '/' . $model->path))) {
-							mkdir(Yii::getAlias('@webroot' . $this->module->public_path . $model->parent_folder . '/' . $model->path), 0755, true);
-						}
-					}
+            if ($model->save()) {
+                if ($this->module->public_path) {
+                    if (!file_exists(Yii::getAlias('@webroot' . '/../..' . $this->module->public_path . $model->parent_folder . '/' . $model->path))) {
+                        mkdir(Yii::getAlias('@webroot' . '/../..' . $this->module->public_path . $model->parent_folder . '/' . $model->path),
+                            0755, true);
+                    }
+                } else {
+                    if (!file_exists(Yii::getAlias('@webroot' . $this->module->public_path . $model->parent_folder . '/' . $model->path))) {
+                        mkdir(Yii::getAlias('@webroot' . $this->module->public_path . $model->parent_folder . '/' . $model->path),
+                            0755, true);
+                    }
+                }
 
-					Yii::$app->session->setFlash('success', 'Folder successfully created.');
+                Yii::$app->session->setFlash('success', 'Folder successfully created.');
 
-					return $this->redirect(['view', 'id' => $model->folder_id]);
-				}
-			}
+                return $this->redirect(['view', 'id' => $model->folder_id]);
+            }
+        }
 
-			return $this->render('create', [
-				'model' => $model,
-			]);
-		}
+        return $this->render('create', [
+            'model' => $model,
+        ]);
+    }
 
-		/**
-		 * Updates an existing Folders model.
-		 * If update is successful, the browser will be redirected to the 'view' page.
-		 *
-		 * @param integer $id
-		 *
-		 * @return mixed
-		 */
-		public function actionUpdate($id)
-		{
-			FilemanagerAsset::register($this->view);
-			$model = $this->findModel($id);
+    /**
+     * Updates an existing Folders model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     *
+     * @param integer $id
+     *
+     * @return mixed
+     */
+    public function actionUpdate($id)
+    {
+        FilemanagerAsset::register($this->view);
+        $model = $this->findModel($id);
 
-			if ($model->load(Yii::$app->request->post()))
-			{
-				$model->parent_folder = trim($model->parent_folder, '/');
-				$model->path = trim($model->path, '/');
+        if ($model->load(Yii::$app->request->post())) {
+            $model->parent_folder = trim($model->parent_folder, '/');
+            $model->path          = trim($model->path, '/');
 
-				if ($model->save())
-				{
-					if ($this->module->public_path)
-					{
-						if (!file_exists(Yii::getAlias('@webroot' . '/../..' . $this->module->public_path . $model->parent_folder . '/' . $model->path))) {
-							mkdir(Yii::getAlias('@webroot' . '/../..' . $this->module->public_path . $model->parent_folder . '/' . $model->path), 0755, true);
-						}
-					}
-					else
-					{
-						if (!file_exists(Yii::getAlias('@webroot' . $model->parent_folder . '/' . $model->path))) {
-							mkdir(Yii::getAlias('@webroot' . $model->parent_folder . '/' . $model->path), 0755, true);
-						}
-					}
-					Yii::$app->session->setFlash('success', 'Folder successfully updated.');
+            if ($model->save()) {
+                if ($this->module->public_path) {
+                    if (!file_exists(Yii::getAlias('@webroot' . '/../..' . $this->module->public_path . $model->parent_folder . '/' . $model->path))) {
+                        mkdir(Yii::getAlias('@webroot' . '/../..' . $this->module->public_path . $model->parent_folder . '/' . $model->path),
+                            0755, true);
+                    }
+                } else {
+                    if (!file_exists(Yii::getAlias('@webroot' . $model->parent_folder . '/' . $model->path))) {
+                        mkdir(Yii::getAlias('@webroot' . $model->parent_folder . '/' . $model->path), 0755, true);
+                    }
+                }
+                Yii::$app->session->setFlash('success', 'Folder successfully updated.');
 
-					return $this->redirect(['view', 'id' => $model->folder_id]);
-				}
-			}
+                return $this->redirect(['view', 'id' => $model->folder_id]);
+            }
+        }
 
-			return $this->render('update', [
-				'model' => $model,
-			]);
-		}
+        return $this->render('update', [
+            'model' => $model,
+        ]);
+    }
 
-		/**
-		 * Deletes an existing Folders model.
-		 * If deletion is successful, the browser will be redirected to the 'index' page.
-		 *
-		 * @param integer $id
-		 *
-		 * @return mixed
-		 */
-		public function actionDelete($id)
-		{
-			$this->findModel($id)->delete();
+    /**
+     * Deletes an existing Folders model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     *
+     * @param integer $id
+     *
+     * @return mixed
+     */
+    public function actionDelete($id)
+    {
+        $baseRoute   = Yii::getAlias('@publicShared') . '/uploads/';
+        $folderModel = $this->findModel($id);
 
-			return $this->redirect(['index']);
-		}
+        var_dump($folderModel);
 
-		/**
-		 * Finds the Folders model based on its primary key value.
-		 * If the model is not found, a 404 HTTP exception will be thrown.
-		 *
-		 * @param integer $id
-		 *
-		 * @return Folders the loaded model
-		 * @throws NotFoundHttpException if the model cannot be found
-		 */
-		protected function findModel($id)
-		{
-			$folders = $this->module->models['folders'];
+        if ($folderModel->parent_folder !== '') {
+            $completeRoute = $baseRoute . $folderModel->parent_folder . '/' . $folderModel->path . '/';
+        } else {
+            $completeRoute = $baseRoute . $folderModel->path . '/';
+        }
 
-			if (($model = $folders::findOne($id)) !== null)
-			{
-				return $model;
-			}
-			else
-			{
-				throw new NotFoundHttpException('The requested page does not exist.');
-			}
-		}
+        if ($this->isDirEmpty($completeRoute)) {
+            rmdir($completeRoute);
+            $folderModel->delete();
+        } else {
+            Yii::$app->getSession()->setFlash(
+                'error',
+                'El directorio no está vacíó. Posee otros directorios dentro o imágenes.'
+            );
+        }
 
-	}
+        return $this->redirect(['index']);
+    }
+
+    private function isDirEmpty($dir)
+    {
+        $handle = opendir($dir);
+        while (false !== ($entry = readdir($handle))) {
+            if ($entry != "." && $entry != "..") {
+                closedir($handle);
+
+                return false;
+            }
+        }
+        closedir($handle);
+
+        return true;
+    }
+
+    /**
+     * Finds the Folders model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     *
+     * @param integer $id
+     *
+     * @return Folders the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        $folders = $this->module->models['folders'];
+
+        if (($model = $folders::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
+
+}
